7af83c26f60a9871a09b22acbab438f736fa5f43
Virtopeanu Sebastian <f_seby@yahoo.com>
2024-03-31 20:09:36 +0300

    Added work

--- main.pl
+++ main.pl
@@ -1,59 +1,173 @@
-% Predicatul principal care inițiază adunarea, cu gestionarea transportului (carry)
-adun(N, M, R) :-
-    reverse(N, NRev),
-    reverse(M, MRev),
-    adun_cifre(NRev, MRev, 0, RRev),
-    reverse(RRev, R).
-
-% Cazul de bază: când ambele liste sunt goale și nu există transport, rezultatul este o listă goală.
-adun_cifre([], [], 0, []) :- !.
-
-% Dacă ambele liste sunt goale dar există un transport, acesta trebuie adăugat la rezultat.
-adun_cifre([], [], Carry, [Carry]) :- Carry > 0.
-
-% Dacă una dintre liste este goală, pur și simplu "transferăm" cifrele și transportul în rezultat.
-adun_cifre([], [M|Ms], Carry, [R|Rs]) :-
-    !, Sum is M + Carry, NextCarry is Sum div 10, R is Sum mod 10,
-    adun_cifre([], Ms, NextCarry, Rs).
-
-adun_cifre([N|Ns], [], Carry, [R|Rs]) :-
-    !, Sum is N + Carry, NextCarry is Sum div 10, R is Sum mod 10,
-    adun_cifre(Ns, [], NextCarry, Rs).
-
-% Cazul general: adunăm cifrele și transportul, apoi continuăm recursiv.
-adun_cifre([N|Ns], [M|Ms], Carry, [R|Rs]) :-
-    Sum is N + M + Carry,
-    NextCarry is Sum div 10,
-    R is Sum mod 10,
-    adun_cifre(Ns, Ms, NextCarry, Rs).
-% Predicatul adun/3
-adun(N, M, R) :- sum_lists(N, M, 0, Res), Res = R.
-
-% Funcție auxiliară care adună două liste cifră cu cifră, gestionând și transportul.
-sum_lists([], [], 0, []) :- !.
-sum_lists([], [], Carry, [Carry]) :- Carry > 0, !.
-sum_lists([H|T], [], Carry, [RH|RT]) :- !,
-    Sum is H + Carry,
-    RH is Sum mod 10,
-    NewCarry is Sum div 10,
-    sum_lists(T, [], NewCarry, RT).
-sum_lists([], [H|T], Carry, [RH|RT]) :- !,
-    sum_lists([H|T], [], Carry, [RH|RT]).
-sum_lists([H1|T1], [H2|T2], Carry, [RH|RT]) :-
-    Sum is H1 + H2 + Carry,
-    RH is Sum mod 10,
-    NewCarry is Sum div 10,
-    sum_lists(T1, T2, NewCarry, RT).
+male(sam).
+male(oliver).
+male(ben).
+male(peter).
+male(roger).
+male(sebi).
+female(elizabeth).
+female(sandra).
+female(mary).
+female(lisa).
+female(olivia).
+parent_of(sandra, sam).
+parent_of(sandra, elizabeth).
+parent_of(sebi, sam).
+parent_of(ben, sam).
+parent_of(ben, elizabeth).
+parent_of(peter, oliver).
+parent_of(peter, sandra).
+parent_of(roger, ben).
+parent_of(roger, lisa).
+parent_of(olivia, roger).
+parent_of(olivia, mary).
 
 
-% Predicatul p1/3 care acceptă A, B și C, verifică sau generează B având A și C.
-p1(A, B, C) :-
-    % Asigurăm că A și C sunt cunoscute (nu sunt variabile neinstantiate)
-    nonvar(A), nonvar(C),
-    % Generăm B știind că A + B = C
-    B is C - A.
+father_of(C, P) :-
+    parent_of(C, P), 
+    male(P).
 
-% Sau, dacă dorim să verificăm suma:
-p1(A, B, C) :-
-    % Verificăm dacă suma lui A și B este egală cu C, presupunând că toate sunt cunoscute
-    C is A + B.
+mother_of(C, P) :-
+    parent_of(C, P), 
+    female(P).
+
+brother_of(C, B) :-
+    parent_of(C, P),
+    parent_of(B, P), 
+    B \= C, 
+    male(B).
+
+sister_of(C, S) :-
+    parent_of(C, P),
+    parent_of(S, P), 
+    S \= C, 
+    female(S).
+
+uncle_of(C, U) :-
+    parent_of(C, P),
+    brother_of(P, U).
+
+aunt_of(C, U) :-
+    parent_of(C, P),
+    sister_of(P, U).
+
+grandfather_of(C, G) :-
+    parent_of(C, P),
+    parent_of(P, G),
+    male(G).
+
+grandmother_of(C, G) :-
+    parent_of(C, P),
+    parent_of(P, G),
+    female(G).
+
+% exercitiul 2
+
+ancestor_of(C, P) :-
+    parent_of(C, P).
+ancestor_of(C, P) :-
+    parent_of(C, P1),
+    ancestor_of(P1, P).
+
+distance((X1, Y1), (X2, Y2), R) :-
+    R is sqrt((X1-X2)**2+(Y1-Y2)**2).
+
+write_aux(1, Sym) :-
+    write(Sym),
+    write('\n'),
+    !.
+write_aux(N, Sym) :-
+    write(Sym),
+    N1 is N-1,
+    write_aux(N1, Sym).
+
+write_n(0, _):-!.
+write_n(N, Sym) :-
+    write_aux(N, Sym),
+    N1 is N-1,
+    write_n(N1, Sym).
+
+write_aux_2(N, I, Sym) :-.
+
+
+write_n_2(N, Sym) :- .
+
+min(X, Y, Y) :-
+    X > Y,
+    !.
+min(X, _, X). 
+
+triangle((X1, Y1), (X2, Y2), (X3, Y3)) :-
+    distance((X1, Y1), (X2, Y2), A),
+    distance((X1, Y1), (X3, Y3), B),
+    distance((X2, Y2), (X3, Y3), C),
+    (   A^2 =:= B^2 + C^2
+    ;   B^2 =:= A^2 + C^2
+    ;   C^2 =:= A^2 + B^2
+    ), 
+    !.
+
+fib_aux(0, _, R, R) :- !.
+fib_aux(N, E1, E2, R) :-
+    E3 is E1+E2,
+    N1 is N-1,
+    fib_aux(N1, E2, E3, R).
+fib(N, R) :- 
+    fib_aux(N, 0, 1, R).
+
+list_length([], 0):-!.
+list_length([_|T], R):-
+    list_length(T, R1),
+    R is R1+1.
+
+list_sum([], 0):-!.
+list_sum([H|T], R):-
+    list_sum(T, R1),
+    R is R1+H.
+
+element_of([H|_], H):-!.
+element_of([_|T], R):-
+    element_of(T, R).
+
+all_a([]).
+all_a([a|T]) :-
+    all_a(T).
+
+trans_a_b([], []).
+trans_a_b([a|T], [b|R]):-
+    trans_a_b(T, R).
+
+scalarMult(_, [], []).
+scalarMult(X, [H|T], [H1|R]):-
+    scalarMult(X, T, R),
+    H1 is H*X.
+
+max_aux([], Max, Max):-!.
+max_aux([H|T], Max, R) :-
+    H>Max, 
+    max_aux(T, H, R).
+max_aux([H|T], Max, R):-
+    H<Max,
+    max_aux(T, Max, R).
+    
+max([H|T], R):-
+    max_aux(T, H, R).
+
+concat_lists([], [], []).
+concat_lists([], [H|T], [H|R]):-
+    concat_lists([], T, R).
+concat_lists([H|T], L, [H|R]):-
+    concat_lists(T, L, R).
+
+
+remove_duplicate([], []).
+remove_duplicate([H|T], [H|R]):-
+    remove_duplicate(T, R),
+    \+ element_of(R, H). 
+remove_duplicate([H|T], R):-
+    remove_duplicate(T, R),
+    element_of(R, H). 
+
+
+
+bagof1(T, Goal, List):-
+    findall(X, (Goal), List).
